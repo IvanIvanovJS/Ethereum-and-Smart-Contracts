b61898634ebfaadb7a3445b61b64c96fbe056e9b
IvanIvanovJS <sibsmartco@gmail.com>
2025-01-25 20:41:24 +0200

    added Discription for the task

--- compoundInterestCalculator.sol
+++ compoundInterestCalculator.sol
@@ -3,6 +3,10 @@
 pragma solidity 0.8.28;
     
 contract compaundInterestCalculator{
+    /*This task involves building a savings account contract that automates compound interest calculations. 
+    The contract calculates the future balance of a principal amount after applying annual compounding interest 
+    over a specified number of years. Each year, 
+    interest is calculated based on the current balance and added for the next year’s calculation.*/
     
     uint256 public constant DECIMALS = 1e18;
     function calculateCompoundInterest(uint256 principal, uint256 interestRate, uint256 timeInYears) external pure returns (uint256) {
